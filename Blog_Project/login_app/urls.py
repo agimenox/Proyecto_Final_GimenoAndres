31d6cb0d348009390a5f0ae887c5fe2b79ebe1d0
Andres Gimeno <andresgimeno_20@hotmail.com>
2023-01-24 17:51:17 -0300

    login aplication

---
+++ Blog_Project/login_app/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from login_app.views import *
+
+
+urlpatterns = [
+
+    
+    path('home/', home, name="home"),
+    #Usuario y sesion
+    path('register/', user_register, name="register"),
+    path('logout/', CustomLogoutView.as_view(), name="logout"),
+    path('login/', login_view, name="login"),
+
+]
---
+++ Blog_Project/login_app/views.py
@@ -0,0 +1,61 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate
+from django.urls import reverse, reverse_lazy
+from login_app.forms import UserRegisterForm
+from django.contrib.auth.views import LogoutView
+from django.contrib.auth.decorators import login_required
+
+# Create your views here.
+
+def login_view(request):
+    next_url = request.GET.get('next')
+    if request.method == "POST":
+        form = AuthenticationForm(request, data=request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+            username = data.get('username')
+            password = data.get('password')
+            user = authenticate(username=username, password=password)
+            # user puede ser un usuario o None
+            if user:
+                login(request=request, user=user)
+                if next_url:
+                    return redirect(next_url)
+                url_exitosa = reverse('home')
+                return redirect(url_exitosa)
+    else:  # GET
+        form = AuthenticationForm()
+    return render(
+        request=request,
+        template_name='login.html',
+        context={'form': form},
+    )
+
+
+def user_register(request):
+    if request.method == "POST":
+        form = UserRegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()  # Esto lo puedo usar porque es un model form
+            success_url = reverse('inicio')
+            return redirect(success_url)
+    else:  # GET
+        form = UserRegisterForm()
+    return render(
+        request=request,
+        template_name='register.html',
+        context={'form': form},
+    )
+
+def home(request):
+    return render(
+        request=request,
+        template_name='panel.html',
+    )
+
+
+class CustomLogoutView(LogoutView):
+    template_name = 'logout.html'
