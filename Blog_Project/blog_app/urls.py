4fa0ae3e70075d2a687bac7ea0b029e7310bd05a
Andres Gimeno <andresgimeno_20@hotmail.com>
2023-01-26 20:12:44 -0300

    added delete for blogs with classbasedviews

--- Blog_Project/blog_app/urls.py
+++ Blog_Project/blog_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from blog_app.views import *
-from .views import BlogCreateView, BlogListView, BlogUpdateView
+from .views import BlogCreateView, BlogListView, BlogUpdateView, BlogDeleteView
 
 
 urlpatterns = [
@@ -8,5 +8,6 @@ urlpatterns = [
     path('create-blog/', BlogCreateView.as_view(), name="create_blog"),
     path('list-blogs/', BlogListView.as_view(), name="list_blogs"),
     path('update-blog/<int:pk>/', BlogUpdateView.as_view(), name="edit_blog"),
+    path('delete-blog/<int:pk>/', BlogDeleteView.as_view(), name="delete_blog"),
 
 ]
--- Blog_Project/blog_app/views.py
+++ Blog_Project/blog_app/views.py
@@ -29,3 +29,8 @@ class BlogUpdateView(UpdateView):
     fields = ['title', 'subtitle','body','author','date','image','category']
     success_url = reverse_lazy('list_blogs')
     template_name = "edit_blog.html"
+
+class BlogDeleteView(DeleteView):
+    model = Blog
+    success_url = reverse_lazy('list_blogs')
+    template_name = "confirm_blog_elimination.html"
